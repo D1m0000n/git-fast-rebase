0a9fbeb9c9e5a85bea63710ef12a5a2d662841a2
D1m0000n <demisrus5554@gmail.com>
2020-05-16 05:39:12 +0400

    Implement changing commit message

--- src/main/java/reword/App.java
+++ src/main/java/reword/App.java
@@ -1,23 +1,28 @@
 package reword;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
+import com.jcraft.jzlib.DeflaterOutputStream;
+
+import org.apache.commons.codec.digest.DigestUtils;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-
 public class App {
     public static void main(String[] args) throws IOException
     {
         String pathToGitFolder = ".git";
-        String inputCommit = "8bee2f73c07e539aaca22211f3f2955e2769be5c";
+        String inputCommit = "HEAD~1";
         
+        String newCommitMessage = "New commit message";
+
         Repository repository = new FileRepositoryBuilder()
         .setGitDir(new File(pathToGitFolder))
         .build();
@@ -32,21 +37,20 @@ public class App {
 
         RevWalk walk = new RevWalk(repository);
 
-        RevCommit commit = walk.parseCommit(repository.resolve(to));
+        RevCommit commit = walk.parseCommit(toCommitId);
 
         System.out.println("Start-Commit: " + commit + "\n");
         System.out.println("Walking all commits starting at " + to + " until we find " + from + "\n");
 
         walk.markStart(commit);
 
-        List<String> commitsContent = new ArrayList<String>();
+        ArrayList<RevCommit> commitsContent = new ArrayList<RevCommit>();
         int count = 0;
 
         for (RevCommit rev : walk) {
             System.out.println("Commit: " + rev);
             count++;
-
-            commitsContent.add(new String(repository.open(rev).getBytes()));
+            commitsContent.add(rev);
 
             if (rev.getId().getName().equals(from)) {
                 System.out.println("\nFound from, stopping walk. " + count);
@@ -56,5 +60,52 @@ public class App {
 
         walk.dispose();
         walk.close();
+
+        String header;
+        RevCommit currentRevCommit;
+        String currentCommitContent;
+        String newCommitHash;
+
+        String prevCommitHash = "";
+
+        ByteArrayOutputStream baos;
+        DeflaterOutputStream dos;
+
+        FileOutputStream outputStream;
+        File file;
+
+        for (int i = commitsContent.size() - 1; i >= 0; i--) {
+            currentRevCommit = commitsContent.get(i);
+            currentCommitContent = new String(currentRevCommit.getRawBuffer());
+
+            if (i == commitsContent.size() - 1) {
+                currentCommitContent = currentCommitContent.replace(currentRevCommit.getFullMessage(), newCommitMessage);
+            } else {
+                currentCommitContent = currentCommitContent.replace("parent " + commitsContent.get(i + 1).getName(), "parent " + prevCommitHash);
+            }
+
+            header = "commit " + currentCommitContent.length() + "\0";
+
+            currentCommitContent = header + currentCommitContent;
+
+            newCommitHash = DigestUtils.sha1Hex(currentCommitContent);
+
+            baos = new ByteArrayOutputStream();
+            dos = new DeflaterOutputStream(baos);
+            dos.write(currentCommitContent.getBytes());
+            dos.flush();
+            dos.close();
+            file = new File(pathToGitFolder + "/objects/" + newCommitHash.substring(0, 2));
+            file.mkdirs();
+            outputStream = new FileOutputStream(pathToGitFolder + "/objects/" + newCommitHash.substring(0, 2) + "/" + newCommitHash.substring(2), false);
+            baos.writeTo(outputStream);
+
+            outputStream.close();
+            baos.reset();
+
+            prevCommitHash = newCommitHash;
+
+            System.out.println(i);
+        }
     }
 }
